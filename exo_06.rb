3bfbe4c0121cf6b2107f70a9763da2262224d0f8
rico <emericthebaut@hotmail.fr>
2019-01-02 07:34:19 -0800

    exo_06.rb

---
+++ exo_06.rb
@@ -0,0 +1,11 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+ 
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+minutes ça fait : #{number_of_minutes_in_an_hour * rnumber_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+# exo_06.rb:6: syntax error, unexpected ':', expecting end-of-input
+minutes ça fait : #{number_of_minutes_in_an_ho...
+#l'éxecution affiche erreur parrcequ'on a pas donné de valeur number_of_minute_in_an_hour et il manque une guillemet
