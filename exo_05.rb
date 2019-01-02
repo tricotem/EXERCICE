409dc21786af2a7162ffbc7fe5841d1e88ac0833
rico <emericthebaut@hotmail.fr>
2019-01-02 07:12:29 -0800

    exo_05.rb

---
+++ exo_05.rb
@@ -0,0 +1,21 @@
+#  #{} permet de rajouter des operations aprés une chaîne de caractère
+puts "0n va compter le nombre d'heures de travail à THP"#chaîne de caractère
+puts "Travail : #{10 * 5 * 11}"# chaîne de caractère + opération
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"# #chaîne de caractère + opération 
+
+puts "Et en seconde ?"# #chaîne de caractère
+
+puts 10 * 5 * 11 * 60 * 60 # opération
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #chaîne de caractère
+
+puts "Ca fait combien 3 + 2 ? #{3 + 2}" # chaîne de caractère + opération
+puts "Ca fait combien 5 - 7 ? #{5 - 7}" # chaîne de caractère + opération
+
+puts "OK, c'est faux alors !"# chaîne de caractère
+
+puts "C'est drôle ça, faisons-en plus :" # chaîne de caractère 
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # chaîne de caractère + operation
+puts "Est-ce que 5 est supérieur ou égale à -2 ? #{5 >= -2}" # #chaîne de caractère + operation
+puts "Est-ce que 5 est inférieur ou égale à -2 ? #{5 <= -2}"# #chaîne de caractère + operation 
