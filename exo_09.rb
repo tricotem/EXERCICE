94b5d9e2d5aa94ab430901177081cbe043e3df07
rico <emericthebaut@hotmail.fr>
2019-01-02 09:22:50 -0800

    exo_09.rb

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour quel est ton prenom ?"
+print"> "
+user_name = gets.chomp
+puts "Bonjour quel est ton nom ?"
+last_name = gets.chomp
+print "Bonjour, " ,  user_name ," ", last_name, " !"
+puts " "
